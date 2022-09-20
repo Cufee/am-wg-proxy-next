4a2951e1e4fc658dc9c896d6e6d0df5d7d61b7fe
Cufee <vkouzin@outlook.com>
2022-09-20 09:28:40 -0400

    removed old vendor

--- internal/vendor/github.com/byvko-dev/am-types/wargaming/generic/api/error.go
+++
@@ -1,8 +0,0 @@
-package api
-
-type Error struct {
-	Message string `json:"message" bson:"message"`
-	Field   string `json:"field" bson:"field"`
-	Value   string `json:"value" bson:"value"`
-	Code    int    `json:"code" bson:"code"`
-}
