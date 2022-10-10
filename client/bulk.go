46b76d549cbcc8890f4cd4d1c735f4001aa39272
Cufee <vkouzin@outlook.com>
2022-10-10 19:26:59 -0400

    Fixed request params

--- client/bulk.go
+++ client/bulk.go
@@ -1,6 +1,9 @@
 package client
 
 import (
+	"strconv"
+	"strings"
+
 	e "github.com/byvko-dev/am-types/errors/v2"
 	"github.com/byvko-dev/am-types/wargaming/v1/accounts"
 	"github.com/byvko-dev/am-types/wargaming/v1/clans"
@@ -9,13 +12,29 @@ import (
 // bulk.Get("/clans/info", query.BulkAccountsInfoHandler)
 func (c *Client) BulkGetClansByID(ids []int, realm string) ([]clans.CompleteProfile, *e.Error) {
 	var target []clans.CompleteProfile
-	return target, c.sendRequest(realm, bulkAccountInfoEndpoint, &target, defaultRequestOptions)
+
+	opts := defaultRequestOptions
+	idsStr := make([]string, len(ids))
+	for i, id := range ids {
+		idsStr[i] = strconv.Itoa(id)
+	}
+	opts.Query.Add("ids", strings.Join(idsStr, ","))
+
+	return target, c.sendRequest(realm, bulkAccountInfoEndpoint, &target, opts)
 }
 
 // bulk.Get("/accounts/info", query.BulkAccountsInfoHandler)
 func (c *Client) BulkGetAccountsByID(ids []int, realm string) ([]accounts.CompleteProfile, *e.Error) {
 	var target []accounts.CompleteProfile
-	return target, c.sendRequest(realm, bulkAccountInfoEndpoint, &target, defaultRequestOptions)
+
+	opts := defaultRequestOptions
+	idsStr := make([]string, len(ids))
+	for i, id := range ids {
+		idsStr[i] = strconv.Itoa(id)
+	}
+	opts.Query.Add("ids", strings.Join(idsStr, ","))
+
+	return target, c.sendRequest(realm, bulkAccountInfoEndpoint, &target, opts)
 }
 
 // bulk.Get("/accounts/vehicles", query.BulkAccountsVehiclesHandler)
