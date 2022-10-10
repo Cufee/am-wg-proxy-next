9c4ad36c533139ead39e6ae2e4051906c4ba7061
Cufee <vkouzin@outlook.com>
2022-10-10 18:05:21 -0400

    cleaned up after cloud run merge, added bulk endpoints

--- vendor/github.com/byvko-dev/am-types/wargaming/v1/accounts/baseprofile.go
+++
@@ -1,6 +0,0 @@
-package accounts
-
-type BaseProfile struct {
-	Nickname  string   `json:"nickname" bson:"nickname"`
-	AccountID PlayerID `json:"account_id" bson:"account_id"`
-}
--- vendor/github.com/byvko-dev/am-types/wargaming/v1/accounts/tokens.go
+++
@@ -1,7 +0,0 @@
-package accounts
-
-type TokenRefreshResponse struct {
-	AccessToken string `json:"access_token"`
-	AccountID   int    `json:"account_id"`
-	Expiration  int    `json:"expires_at"`
-}
