38b1d43533f0ac3b84c6d9e6e1677ea8b808e144
Vovko <vkouzin@outlook.com>
2024-12-04 12:03:18 -0500

    simplified realm checks

--- internal/utils/realm.go
+++ internal/utils/realm.go
@@ -1,36 +1,24 @@
 package utils
 
 import (
-	"slices"
 	"strconv"
 	"strings"
 
 	"github.com/cufee/am-wg-proxy-next/v2/types"
 )
 
-var minRegionIDs = map[types.Realm]int{
-	types.RealmBotAccounts:  42e8,
-	types.RealmChina:        31e8,
-	types.RealmAsia:         20e8,
-	types.RealmNorthAmerica: 10e8,
-	types.RealmEurope:       5e8,
-	types.RealmRussia:       0e8,
-}
-
 type realmParams struct {
 	realm types.Realm
 	min   int
 }
 
-var regions []realmParams
-
-func init() {
-	for realm, min := range minRegionIDs {
-		regions = append(regions, realmParams{realm, min})
-	}
-	slices.SortFunc(regions, func(a, b realmParams) int {
-		return b.min - a.min
-	})
+var regions = []realmParams{
+	{realm: types.RealmBotAccounts, min: 42e8},
+	{realm: types.RealmChina, min: 31e8},
+	{realm: types.RealmAsia, min: 20e8},
+	{realm: types.RealmNorthAmerica, min: 10e8},
+	{realm: types.RealmEurope, min: 5e8},
+	{realm: types.RealmRussia, min: 0e8},
 }
 
 func RealmFromID(idStr string) *types.Realm {
